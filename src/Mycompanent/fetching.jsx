546e993dccbc4556c9f3189142be6493a93646dc
BabariyaMit8702 <mit944996@gmail.com>
2025-08-03 13:36:44 +0530

    learn useeffet and apis

--- src/Mycompanent/fetching.jsx
+++ src/Mycompanent/fetching.jsx
@@ -4,11 +4,11 @@ import { useEffect } from 'react'
 
 export const Fetch = () => {
     const [dta, setdta] = useState(0);
-
+    const [sr_no, setsr_no] = useState(1)
     useEffect(() => {
-      async function called () {
+      async function called (k) {
         try{
-        let response = await fetch('https://jsonplaceholder.typicode.com/posts/9');
+        let response = await fetch(`https://jsonplaceholder.typicode.com/posts/${k}`);
         if(!response.ok){
             throw new Error('network issues');
         }
@@ -18,12 +18,24 @@ export const Fetch = () => {
         setdta(err.message);
     }
       }
-      called();
-    }, [])
+      document.title = 'API OF id No. ' + sr_no
+      called(sr_no);
+    }, [sr_no])
     
-    
+    function inc () {
+      setsr_no(sr_no+1);
+    }
+    function dec () {
+      setsr_no(sr_no-1);
+    }
   return (
     <>
+    <div>
+      <h3>{sr_no}</h3>
+      <button className='btn btn-primary' onMouseOver={inc}>+</button>&nbsp;&nbsp;
+      <button className='btn btn-danger' onMouseOver={sr_no>1 && dec}>-</button>
+      <hr></hr>
+    </div>
         <div>
             {dta===0? <h4>Loading</h4> :
                 <>
